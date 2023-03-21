07cfd5f4d9ee62e25a49610635af2223e93a0e9a
UsernameDMG <daniel.mgutierrez@campusfp.es>
2023-03-21 11:10:05 +0100

    "pruebas caja negra"

--- src/test/Pruebas.java
+++ src/test/Pruebas.java
@@ -11,8 +11,27 @@ class Pruebas {
 	CuentaBancaria cb = new CuentaBancaria();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test() throws Exception{
+		CuentaBancaria CB = new CuentaBancaria();
+		CB.agregarSaldo(2000);
+		CB.modificarCredito(1000);
+		assertEquals(2000, CB.obtenerSaldo());
+	}
+	
+	@Test
+	void test1() throws Exception{
+		CuentaBancaria CB = new CuentaBancaria();
+		CB.modificarCredito(900);
+		CB.retirarSaldo(500);
+		assertEquals(-500, CB.obtenerSaldo());
+	}
+	
+	@Test
+	void test2() throws Exception{
+		CuentaBancaria CB = new CuentaBancaria();
+		CB.modificarCredito(1000);
+		CB.retirarSaldo(800);
+		assertThrows(Exception.class, () -> CB.modificarCredito(500));
 	}
 
 }
